12f9c90b50f05887904d8adc0af73263296e7ef7
Zhang JC <632897649@qq.com>
2021-08-10 19:39:23 +0800

    add dvorak and qwerty layer

--- dvorak_layout/dvorak_layout.ino
+++ dvorak_layout/dvorak_layout.ino
@@ -18,24 +18,45 @@
 
 /* *INDENT-OFF* */
 KEYMAPS(
-  [0] = KEYMAP_STACKED
+  // DVORAK
+  [DVORAK] = KEYMAP_STACKED
   (
-   Key_NoKey,    Key_1, Key_2, Key_3, Key_4, Key_5, Key_NoKey,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   Key_Backtick,    Key_1,         Key_2,       Key_3,        Key_4, Key_5, Key_NoKey,
+   Key_Tab,         Key_Quote,     Key_Comma,   Key_Period,   Key_P, Key_Y, Key_NoKey,
+   Key_LeftControl, Key_A,         Key_O,       Key_E,        Key_U, Key_I,
+   Key_LeftShift,   Key_Semicolon, Key_Q,       Key_J,        Key_K, Key_X, Key_NoKey,
 
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   Key_Escape,      Key_LeftAlt,   Key_LeftGui, Key_Spacebar,
    Key_NoKey,
 
-   Key_skip,  Key_6, Key_7, Key_8,     Key_9,      Key_0,         Key_skip,
-   Key_Enter, Key_Y, Key_U, Key_I,     Key_O,      Key_P,         Key_Equals,
-              Key_H, Key_J, Key_K,     Key_L,      Key_Semicolon, Key_Quote,
-   Key_skip,  Key_N, Key_M, Key_Comma, Key_Period, Key_Slash,     Key_Minus,
+   Key_NoKey,       Key_6,         Key_7,       Key_8,        Key_9, Key_0, Key_Slash,
+   Key_NoKey,       Key_F,         Key_G,       Key_C,        Key_R, Key_L, Key_Backslash,
+   ,                Key_D,         Key_H,       Key_T,        Key_N, Key_S, Key_Minus,
+   Key_NoKey,       Key_B,         Key_M,       Key_W,        Key_V, Key_Z, Key_Equals,
 
-   Key_RightShift, Key_RightAlt, Key_Spacebar, Key_RightControl,
+   Key_Backspace,   Key_Enter,     Key_NoKey,   Key_NoKey,
    Key_NoKey
   ),
+
+  // QWERTY
+  [QWERTY] = KEYMAP_STACKED
+  (
+   Key_Backtick,    Key_1,       Key_2,       Key_3,        Key_4,      Key_5,         Key_NoKey,
+   Key_Tab,         Key_Q,       Key_W,       Key_E,        Key_R,      Key_T,         Key_NoKey,
+   Key_LeftControl, Key_A,       Key_S,       Key_D,        Key_F,      Key_G,
+   Key_LeftShift,   Key_Z,       Key_X,       Key_C,        Key_V,      Key_B,         Key_NoKey,
+
+   Key_Escape,      Key_LeftAlt, Key_LeftGui, Key_Spacebar,
+   Key_NoKey,
+
+   Key_NoKey,       Key_6,       Key_7,       Key_8,        Key_9,      Key_0,         Key_NoKey,
+   Key_NoKey,       Key_Y,       Key_U,       Key_I,        Key_O,      Key_P,         Key_Equals,
+                    Key_H,       Key_J,       Key_K,        Key_L,      Key_Semicolon, Key_Quote,
+   Key_NoKey,       Key_N,       Key_M,       Key_Comma,    Key_Period, Key_Slash,     Key_Minus,
+
+   Key_Backspace,   Key_Enter,   Key_NoKey,   Key_NoKey,
+   Key_NoKey
+  )
 )
 /* *INDENT-ON* */
 
